dd9a84d06aec3d8339d8445c4eefb68301409842
Nizam Abdul <nizamabdul@makinus.com>
2019-05-15 15:54:00 +0530

    Updated Source File

--- src/main/java/com/example/demo/flyway/DataCleanMigration.java
+++ src/main/java/com/example/demo/flyway/DataCleanMigration.java
@@ -1,12 +1,3 @@
-/*
- *
- *    Copyright (c) 2017 Makinus Pvt Ltd - All Rights Reserved
- *
- *    Unauthorized copying of this file, via any medium is strictly prohibited
- *    Proprietary and confidential
- *    Written by Makinus Pvt Ltd
- *
- */
 package com.example.demo.flyway;
 
 import org.apache.logging.log4j.LogManager;
@@ -14,9 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
-/**
- * Created by abuabdul
- */
 public class DataCleanMigration implements FlywayMigrationStrategy {
 
     private final Logger LOG = LogManager.getLogger(DataCleanMigration.class);
