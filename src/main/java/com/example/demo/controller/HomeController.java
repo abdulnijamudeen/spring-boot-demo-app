7d3d71049d9d3eef84ce7960f932bf9500f1cd35
Nizam <younsahamed@gmail.com>
2019-08-30 23:00:49 +0530

    Layout for home and about

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,44 +6,53 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private static final String HOME_PAGE = "site/index";
+    private static final String ABOUTUS_PAGE = "site/about";
+    private static final String SERVICE_PAGE = "site/service";
+    private static final String PROJECT_PAGE = "site/project";
+    private static final String BLOG_PAGE = "site/blog";
+    private static final String SINGLE_BLOG_PAGE = "site/single-blog";
+    private static final String ELEMENTS_PAGE = "site/elements";
+    private static final String CONTACT_PAGE = "site/contact";
+
     @GetMapping(value = {"/", "/index"})
     public String index() {
-        return "site/index";
+        return HOME_PAGE;
     }
 
     @GetMapping("/about")
     public String about() {
-        return "site/about";
+        return ABOUTUS_PAGE;
     }
 
     @GetMapping("/service")
     public String service() {
-        return "site/service";
+        return SERVICE_PAGE;
     }
 
     @GetMapping("/project")
     public String project() {
-        return "site/project";
+        return PROJECT_PAGE;
     }
 
     @GetMapping("/blog")
     public String blog() {
-        return "site/blog";
+        return BLOG_PAGE;
     }
 
     @GetMapping("/single-blog")
     public String singleBlog() {
-        return "site/single-blog";
+        return SINGLE_BLOG_PAGE;
     }
 
     @GetMapping("/elements")
     public String elements() {
-        return "site/elements";
+        return ELEMENTS_PAGE;
     }
 
     @GetMapping("/contact")
     public String contact() {
-        return "site/contact";
+        return CONTACT_PAGE;
     }
 
 }
